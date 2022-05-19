831d1adc4895377f1c661b71fa0f16c3f6c0ab45
LENOVO@LAPTOP_IAP0UF05 <LENOVO@LAPTOP-IAP0UF05>
2022-05-19 15:07:06 +0700

    update

--- aseets/read.js
+++ aseets/read.js
@@ -1,24 +1,24 @@
 Vue.createApp({
   data() {
     return {
-      article: null,
+      articles: null,
     };
   },
   methods: {
     getMarkdownData() {
       const queryString = window.location.search;
       const urlParams = new URLSearchParams(queryString);
-      const article = urlParams.get("article");
+      const articles = urlParams.get("articles");
       var converter = new showdown.Converter();
       console.log(article);
       axios
         .get(
           "https://raw.githubusercontent.com/Retnoayuwulandari/tekweb2022/master/contents/" +
-            article
+            articles
         )
         .then((res) => {
           var html = converter.makeHtml(res.data);
-          this.article = html;
+          this.articles = html;
           console.log(html);
         })
         .catch((error) => {
